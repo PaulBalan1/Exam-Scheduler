9893c80cd6a0a1fd6bf77e362ca607eeb97192ab
PaulBalan1 <56250869+PaulBalan1@users.noreply.github.com>
2019-12-01 17:30:28 +0100

    Examiners

---
+++ Software/src/Examiner.java
@@ -0,0 +1,38 @@
+public class Examiner {
+    private String name;
+    private Course course;
+
+    public Examiner(String name, Course course){
+        setName(name);
+        setCourse(course);
+    }
+
+    public Examiner(String name){
+        setName(name);
+        setCourse(null);
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Examiner{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+}
---
+++ Software/src/ExaminerList.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class ExaminerList {
+    private ArrayList<Examiner> examiners = null;
+
+    public ExaminerList(){
+        examiners = new ArrayList<Examiner>();
+    }
+
+    public void addExaminer(Examiner examiner){
+        examiners.add(examiner);
+    }
+
+    public void addExaminer(String name){
+        Examiner aux = new Examiner(name);
+        examiners.add(aux);
+    }
+
+    public void addExaminer(String name, Course course){
+        Examiner aux = new Examiner(name, course);
+        examiners.add(aux);
+    }
+
+    public void removeExaminer(Examiner examiner){
+        examiners.remove(examiner);
+    }
+
+    public void removeExaminer(String name){
+        for(Examiner examiner: examiners){
+            if(examiner.getName().equals(name)){
+                examiners.remove(examiner);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String aux = "";
+        for (Examiner examiner: examiners)
+        {
+            aux += examiner + "\n";
+        }
+        return aux;
+    }
+}
