6151b693f2bb9b146bbc542482f298334a1e80ef
PaulBalan1 <56250869+PaulBalan1@users.noreply.github.com>
2019-12-04 15:35:11 +0100

    Test3

--- Software/src/Interface/test.java
+++ Software/src/Interface/test.java
@@ -11,14 +11,8 @@ public class  test extends Application
   @Override
   public void start(Stage primaryStage) throws Exception{
     FXMLLoader loader = new FXMLLoader();
-<<<<<<< HEAD:Software/src/test.java
-    loader.setLocation(getClass().getResource("FxmlFiles/MainMenu.fxml"));
-    Parent root = loader.load();
-    //MainMenu model = new MainMenu();
-=======
     loader.setLocation(getClass().getResource("MainMenu.fxml"));
     Parent root = loader.load();
->>>>>>> e14e164b424e4989ff0e0ceb86b1addb0e72a5fc:Software/src/Interface/test.java
     primaryStage.setTitle("Exam Scheduler");
     primaryStage.setScene(new Scene(root, 700, 500));
     primaryStage.show();
